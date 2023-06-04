c40a55a9ad8f35ba4c9843a0cc93faa1a1cedc65
i44p <jsbbdjzjsjawk+@gmail.com>
2023-06-04 23:11:32 +0300

    fix big numbers

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,11 +6,15 @@
 
 #include "tree.h"
 
+int factorial(int n) { return n ? n * factorial(n - 1) : 1; }
+
 std::vector<char> getPerm(const Tree& tree, int n) {
+  if (n > factorial(tree.strv.size())) return std::vector<char>();
+
   std::vector<char> str = tree.strv;
 
-  for (int count = 1; count < n; count++)
-    std::next_permutation(str.begin(), str.end());
+  for (; n > 1 && std::next_permutation(str.begin(), str.end()); n--)
+    ;
 
   return str;
 }
